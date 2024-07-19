d54e38beb51d1991ab1b9a672fc682fcee13ae24
David Michel <david.michel2809@gmail.com>
2024-07-19 17:15:47 +0200

    add openai

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import express, { type Express, type Request, type Response } from "express";
 import dotenv from "dotenv";
+import OpenAI from "openai";
 
 dotenv.config();
+const openai = new OpenAI({
+  organization: process.env.ORGANIZATION_ID,
+  project: process.env.PROJECT_ID,
+});
+
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", async (req: Request, res: Response) => {
+
   res.json({ message: "Express + TypeScript Server" });
 });
 
